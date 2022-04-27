9450b6fefddacc51627e8a823184a501b956d572
Gonzalo Hernán Perez <102616078+GonzaPe@users.noreply.github.com>
2022-04-27 19:34:14 -0300

    Javascript042701
    Se crean documentos para probar funciones de JS corriendo para docs HTML

---
+++ Javascript-042701.js
@@ -0,0 +1,7 @@
+console.log("hola mundo")
+document.write("Escribo sobre el documento desde el archivo .js")
+function multiplicar( numero1, numero2){
+    var resultado = numero1 * numero2
+    document.write("el resultado es ",
+    resultado,"<br>")
+}
